6a17da10c0b1dab00ec565c01bba0281415fb7bf
shashibrvsingh <shashi.brvsingh@gmail.com>
2024-12-29 20:22:02 +0530

    commit

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  console.log(selectedMeal);
   return (
     <View>
-      <Text>This is meal detail screen: {mealId}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title} </Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}> {step}</Text>
+      ))}
     </View>
   );
 }
